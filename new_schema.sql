6d97c96dd70e30a9f550e5942bd6ee555c540d9d
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-08-24 13:16:28 -0700

    migrate the data

--- new_schema.sql
+++ new_schema.sql
@@ -78,12 +78,67 @@ DROP TABLE IF EXISTS "votes"
 -- e.Make sure that a given user can only vote once on a given post:
     -- i.	Hint: you can store the (up/down) value of the vote as the values 1 and -1 respectively.
     -- ii.	If the user who cast a vote gets deleted, then all their votes will remain, but will become dissociated from the user.
+    -- iii.	If a post gets deleted, then all the votes for that post should be automatically deleted too.
     CREATE TABLE "votes"(
         "id" SERIAL,
         "user_id" INTEGER REFERENCES "users" ON DELETE SET NULL,
-        "post_id" INTEGER,
+        "post_id" INTEGER REFERENCES "posts" ON DELETE CASCADE,
         "topic_id" INTEGER,
         "vote" INTEGER,
         CONSTRAINT "plus_one_minus_one_vote" CHECK("vote" = 1 or "vote"= -1),
         PRIMARY KEY("user_id","post_id")
-    )
+    );
+
+
+-- Migrate all the data(users)
+INSERT INTO "users" ("username")
+SELECT DISTINCT "username"
+FROM "bad_posts";
+
+UNION ALL
+
+INSERT INTO "users" ("username")
+SELECT DISTINCT "username"
+FROM "bad_comments"
+
+UNION ALL
+
+SELECT regex_split_to_table(upvotes,',')
+FROM bad_posts
+
+UNION ALL
+
+SELECT regex_split_to_table(downvotes,',')
+FROM bad_posts;
+
+
+
+-- Migrate all the data(posts)
+INSERT INTO "posts"("id","title", "URL", "text_content", "topic_id", "user_id")
+SELECT bad_posts."id",
+       LEFT(bad_posts."title",100),
+       bad_posts."url",
+       bad_posts."text_content",
+       topics."id",
+       users."id"
+FROM "bad_posts"
+INNER JOIN "users"
+ON users."username" = bad_posts."username"
+INNER JOIN "topics"
+ON topics."name" = bad_posts."topic"
+
+
+
+-- Migrate all the data(comments)
+INSERT INTO "comments"("id","post_id", "user_id", "text_content")
+SELECT bad_comments."id",
+       posts."id",
+       users."id",
+       bad_comments."text_content"
+FROM "bad_comments"
+INNER JOIN "posts"
+ON posts."id" = bad_comments."post_id"
+INNER JOIN "users"
+ON users."username" = bad_comments."username"
+
+
